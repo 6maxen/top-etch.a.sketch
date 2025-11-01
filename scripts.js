ac0e51f153d067aba9eed419f7b7754c3ad6c616
max1 <240841057+6maxen@users.noreply.github.com>
2025-11-01 01:17:37 -0400

    Add Grid-Size Functionality and Placeholder-Clear-Button

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 
 function createGrid(column, row, squareSize) {
     let container = document.querySelector("#container");
+    container.innerHTML = "";
     container.style.width = `${column * squareSize}px`
 
     for (let i=0; i<column; i++) {
@@ -20,5 +21,18 @@ function createGrid(column, row, squareSize) {
     }
 }
 
-createGrid(16, 16, 30)
+createGrid(16,16,30)
 
+columnInput = document.querySelector("#columnInput");
+rowInput = document.querySelector("#rowInput");
+sizeInput = document.querySelector("#sizeInput");
+gridSubmit = document.querySelector("#gridSubmit");
+
+    
+gridSubmit.addEventListener("click", () => {
+    let columnValue = Number(columnInput.value);
+    let rowValue = Number(rowInput.value);
+    let sizeValue = Number(sizeInput.value);
+
+    createGrid(columnValue, rowValue, sizeValue)
+})
